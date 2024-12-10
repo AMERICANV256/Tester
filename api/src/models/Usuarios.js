1efc886d39e1bba07d84d4b633c91800a238b7e6
Matu36 <matipineda85@live.com.ar>
2024-12-10 12:07:43 -0300

    jwt

---
+++ api/src/models/Usuarios.js
@@ -0,0 +1,87 @@
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Usuarios",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notEmpty: true,
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      nombre: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+      },
+      apellido: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+      },
+      direccion: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+      },
+      telefono: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+      },
+      rol: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: null,
+      },
+      activo: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false,
+      },
+      firma: {
+        type: DataTypes.TEXT,
+        defaultValue: null,
+      },
+      suscripcion: {
+        type: DataTypes.STRING,
+        defaultValue: "basic",
+      },
+      fechaInicioSuscripcion: {
+        type: DataTypes.DATE,
+        defaultValue: null,
+      },
+      fechaFinSuscripcion: {
+        type: DataTypes.DATE,
+        defaultValue: null,
+      },
+      pagoActivo: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      metodoPago: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+      },
+      moneda: {
+        type: DataTypes.STRING,
+        defaultValue: "USD",
+      },
+      pais: {
+        type: DataTypes.STRING,
+        defaultValue: null,
+      },
+    },
+    {
+      updatedAt: false,
+    }
+  );
+};
